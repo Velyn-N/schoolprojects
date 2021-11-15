1e524e8da7f4bf5ff6d1d323bd95f70938fc3bff
Nia Tahl <eliyanbrize@gmail.com>
2021-11-15 19:54:17 +0100

    Display in Aachener stil

--- Shapes/src/main/java/de/nmadev/shapes/Display.java
+++ Shapes/src/main/java/de/nmadev/shapes/Display.java
@@ -1,5 +1,7 @@
 package de.nmadev.shapes;
 
+import de.nmadev.WebOut;
+
 public class Display {
 	
 	public Display() {}
@@ -7,11 +9,18 @@ public class Display {
 	public double getLength(Line line) {
 		Point p1 = line.getPoint1();
 		Point p2 = line.getPoint2();
-		return Math.sqrt( Math.pow((p2.getX()-p1.getX()),2) + Math.pow(p2.getY(), p1.getY()) );
+
+		double res = Math.sqrt( Math.pow((p2.getX()-p1.getX()),2) + Math.pow(p2.getY(), p1.getY()) );
+
+		WebOut.getInstance().write("Länge: " + res);
+		return res;
 	}
 	
 	public double getArea(Circle circle) {
-		return Math.pow(circle.getRadius(),2) * Math.PI;
+		double res = Math.pow(circle.getRadius(),2) * Math.PI;
+
+		WebOut.getInstance().write("Fläche: " + res);
+		return res;
 	}
 	
 	public double getArea(Rectangle rectangle) {
@@ -19,10 +28,14 @@ public class Display {
 		Point p2 = rectangle.getPoint2();
 		Point p3 = rectangle.getPoint3();
 		Point p4 = rectangle.getPoint4();
-		return 0.5 * ( 
+
+		double res = 0.5 * ( 
 			(p1.getX()*p2.getX() - p1.getY()*p2.getY()) +
 			(p2.getX()*p3.getX() - p2.getY()*p3.getY()) +
 			(p3.getX()*p4.getX() - p3.getY()*p4.getY()) +
 			(p4.getX()*p1.getX() - p4.getY()*p1.getY()) );
+
+		WebOut.getInstance().write("Fläche: " + res);
+		return res;
 	}		
 }
