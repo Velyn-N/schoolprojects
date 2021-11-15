a917ee07fcdf9bb66a10ba3d2c87c045d5bfa864
Nia Tahl <eliyanbrize@gmail.com>
2021-11-15 17:37:26 +0100

    MATH!!!

--- Shapes/src/main/java/de/nmadev/shapes/Display.java
+++ Shapes/src/main/java/de/nmadev/shapes/Display.java
@@ -4,15 +4,25 @@ public class Display {
 	
 	public Display() {}
 	
-	public int getLength(Line line) {
-		return 0;
+	public double getLength(Line line) {
+		Point p1 = line.getPoint1();
+		Point p2 = line.getPoint2();
+		return Math.sqrt( Math.pow((p2.getX()-p1.getX()),2) + Math.pow(p2.getY(), p1.getY()) );
 	}
 	
-	public int getArea(Circle circle) {
-		return 0;
+	public double getArea(Circle circle) {
+		return Math.pow(circle.getRadius(),2) * Math.PI;
 	}
 	
-	public int getArea(Rectangle rectangle) {
-		return 0;
-	}
+	public double getArea(Rectangle rectangle) {
+		Point p1 = rectangle.getPoint1();
+		Point p2 = rectangle.getPoint2();
+		Point p3 = rectangle.getPoint3();
+		Point p4 = rectangle.getPoint4();
+		return 0.5 * ( 
+			(p1.getX()*p2.getX() - p1.getY()*p2.getY()) +
+			(p2.getX()*p3.getX() - p2.getY()*p3.getY()) +
+			(p3.getX()*p4.getX() - p3.getY()*p4.getY()) +
+			(p4.getX()*p1.getX() - p4.getY()*p1.getY()) );
+	}		
 }
